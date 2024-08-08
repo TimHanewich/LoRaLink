3967e69441e0a89a3600cf3c6b86e7346100940f
Tim Hanewich <tahanewich@live.com>
2024-08-08 09:48:14 -0400

    Condensed to value

--- src/bincomms.py
+++ src/bincomms.py
@@ -1,11 +1,4 @@
 import binary
 
-@property
-def pulse_call() -> int:
-    """Returns the single-byte representation of a pulse call."""
-    return binary.bits_to_byte([True, False, False, False, False, False, False, False])
-
-@property
-def pulse_echo() -> int:
-    """Returns the single-byte representation of a pulse echo."""
-    return binary.bits_to_byte([True, True, False, False, False, False, False, False])
+pulse_call = binary.bits_to_byte([True, False, False, False, False, False, False, False]) # Single-byte representation of a pulse call.
+pulse_echo = binary.bits_to_byte([True, True, False, False, False, False, False, False]) # Single-byte representation of a pulse echo.
