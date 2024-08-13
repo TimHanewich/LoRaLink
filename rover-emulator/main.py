04fc7c28ca5a1ac7df7ae67fbf204de85efc21ac
Tim Hanewich <tahanewich@live.com>
2024-08-12 20:28:53 -0400

    Emulator also sends battery

--- rover-emulator/main.py
+++ rover-emulator/main.py
@@ -2,6 +2,7 @@ import reyax
 import machine
 import time
 import bincomms
+import random
 
 u = machine.UART(0, tx=machine.Pin(16), rx=machine.Pin(17), baudrate=115200)
 lora = reyax.RYLR998(u)
@@ -16,8 +17,12 @@ lora.band = 960000000 # set band to highest (fastest)
 lora.rf_parameters = (7, 9, 1, 8) # Spreadig Factor of 7, Bandwidth of 500 KHz, Coding Rate of 1, Programmed Preamble of 8
 print("Configured!")
 
+# infinite loop
+operational_status_last_sent:int = time.ticks_ms()
 while True:
     print("Trying to receive a message...")
+
+    # try to receive
     rm:reyax.ReceivedMessage = lora.receive()
     if rm != None:
         print("Recieved: " + str(rm))
@@ -31,5 +36,14 @@ while True:
             print("It is an operational command: " + str(opcmd))
         else:
             print("But it wasn't a pulse call!")
+
+    # time to send out op status?
+    if (time.ticks_ms() - operational_status_last_sent) > 8000:
+        battery:float = random.randint(0, 100) / 100
+        opstatus:bincomms.OperationalResponse = bincomms.OperationalResponse()
+        opstatus.battery = battery
+        lora.send(0, opstatus.encode())
+        print("Just sent '" + str(opstatus.encode()) + "'!")
+        operational_status_last_sent = time.ticks_ms()
 
     time.sleep_ms(100)
